661882cb68e291cd65385106ee336aa42874ab22
Bernard Ginn Jr. <117537210+BG-legacy@users.noreply.github.com>
2024-10-09 19:47:18 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBne4WwI6e5QPUjNHEJB-teqboWBgRe320",
-    authDomain: "recipe-recommender-9d84c.firebaseapp.com",
-    projectId: "recipe-recommender-9d84c",
-    storageBucket: "recipe-recommender-9d84c.appspot.com",
-    messagingSenderId: "577481829789",
-    appId: "1:577481829789:web:bed183fda10927da3f5374",
-    measurementId: "G-5F38PE6C27"
+    apiKey: ""
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };  
 
 // Initialize Firebase
